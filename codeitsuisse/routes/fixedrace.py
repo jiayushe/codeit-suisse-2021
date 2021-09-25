d146c1bde0e742db1f06e2e9cde0ec3a889c6d6b
jiayushe <shejiayu1998@gmail.com>
2021-09-25 21:35:13 +0800

    Update fixedrace

--- codeitsuisse/routes/fixedrace.py
+++ codeitsuisse/routes/fixedrace.py
@@ -14,6 +14,21 @@ def evaluateFixedrace():
     return result
 
 def solve(data):
+    weight = {
+        "Zada Zynda": 20,
+        "Justin Jack": 18,
+        "Fabian Fogel": 16,
+        "Jared Jinkins": 14,
+        "Anibal Abler": 12,
+        "Nelson Noss": 10,
+        "Shelli Scheuerman": 10,
+        "Gilberto Gethers": 10,
+        "Vida Veal": 10,
+        "Annalee Angert": 10
+    }
     arr = data.split(",")
-    random.shuffle(arr)
-    return ','.join(arr)
+    for i in arr:
+        if i not in weight:
+            weight[i] = random.randint(0, 10)
+    result = sorted(arr, key=lambda x: -weight[x])
+    return ','.join(result)
