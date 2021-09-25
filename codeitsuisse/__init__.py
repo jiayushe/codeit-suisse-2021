a33c88125b34306fba83f28e2d68b466ca3239ca
jiayushe <shejiayu1998@gmail.com>
2021-09-25 15:07:06 +0800

    Add asteroid

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -5,3 +5,4 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.secret_message
 import codeitsuisse.routes.parasite
+import codeitsuisse.routes.asteroid
---
+++ codeitsuisse/routes/asteroid.py
@@ -0,0 +1,63 @@
+import logging
+from flask import request, jsonify
+from codeitsuisse import app
+
+logger = logging.getLogger(__name__)
+
+@app.route('/asteroid', methods=['POST'])
+def evaluateAsteroid():
+    data = request.get_json()
+    logging.info("data sent for evaluation {}".format(data))
+    test_cases = data.get("test_cases")
+    result = []
+    for test_case in test_cases:
+        logging.info("evaluating {}".format(test_case))
+        ans = {}
+        ans["input"] = test_case
+        solve(ans, test_case)
+        result.append(ans)
+    logging.info("My result :{}".format(result))
+    return jsonify(result)
+
+def score(n):
+    if n < 7:
+        return n
+    elif n < 10:
+        return n * 1.5
+    else:
+        return n * 2
+
+def solve(ans, s):
+    n = len(s)
+    arr = []
+    x = 0
+    while x < n:
+        let = s[x]
+        y = x + 1
+        while y < n and let == s[y]:
+            y += 1
+        arr.append((let, x, y))
+        x = y
+    m = len(arr)
+    best_sum = -1
+    best_ori = -1
+    for i in range(m):
+        let, x, y = arr[i]
+        if y - x < 3:
+            continue
+        curr_sum = 0
+        curr_sum += score(y - x)
+        prev, nxt = i - 1, i + 1
+        while prev >= 0 and nxt < m:
+            prev_let, prev_x, prev_y = arr[prev]
+            nxt_let, nxt_x, nxt_y = arr[nxt]
+            if prev_let != nxt_let:
+                break
+            curr_sum += score(prev_y - prev_x + nxt_y - nxt_x)
+            prev -= 1
+            nxt += 1
+        if curr_sum > best_sum:
+            best_sum = curr_sum
+            best_ori = (x + y) // 2
+    ans["score"] = best_sum
+    ans["origin"] = best_ori
