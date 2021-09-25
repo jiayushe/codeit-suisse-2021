498fe45ee82b6e8def920ceb5ffd89d65e8c9af5
jiayushe <shejiayu1998@gmail.com>
2021-09-25 23:10:41 +0800

    Fix stock-hunter

--- codeitsuisse/routes/stocker_hunter.py
+++ codeitsuisse/routes/stocker_hunter.py
@@ -21,22 +21,23 @@ def valid(x, y, row, col):
     return x >= 0 and x < row and y >= 0 and y < col
 
 def solve(data):
-    start = [data["entryPoint"]["first"], data["entryPoint"]["second"]]
-    target = [data["targetPoint"]["first"], data["targetPoint"]["second"]]
+    # (Y, X)
+    start = [data["entryPoint"]["second"], data["entryPoint"]["first"]]
+    target = [data["targetPoint"]["second"], data["targetPoint"]["first"]]
     depth = data["gridDepth"]
     key = data["gridKey"]
     hor = data["horizontalStepper"]
     ver = data["verticalStepper"]
-    row, col = target[0] * 2, target[1] * 2
+    row, col = target[0] * 3, target[1] * 3
     risk_level = [[0 for j in range(col)] for i in range(row)]
     for i in range(row):
         if start == [i, 0] or target == [i, 0]:
             continue
-        risk_level[i][0] = (i * hor + depth) % key
+        risk_level[i][0] = (i * ver + depth) % key
     for i in range(col):
         if start == [0, i] or target == [0, i]:
             continue
-        risk_level[0][i] = (i * ver + depth) % key
+        risk_level[0][i] = (i * hor + depth) % key
     for i in range(1, row):
         for j in range(1, col):
             if start == [i, j] or target == [i, j]:
@@ -54,16 +55,20 @@ def solve(data):
                 grid_map[i][j] = "S"
     ans = {}
     ans["gridMap"] = grid_map
+    visited = [[0 for j in range(col)] for i in range(row)]
     q = PriorityQueue()
     q.put((0, start[0], start[1]))
     dir = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     while not q.empty():
         c, x, y = q.get()
+        if visited[x][y] == 1:
+            continue
+        visited[x][y] = 1
         if x == target[0] and y == target[1]:
             ans["minimumCost"] = c
             break
         for dx, dy in dir:
             nx, ny = x + dx, y + dy
-            if valid(nx, ny, row, col):
+            if valid(nx, ny, row, col) and visited[nx][ny] == 0:
                 q.put((c + risk_cost[nx][ny], nx, ny))
     return ans
