644465f4a138274c9f4c61c0945efa0c3122a241
jiayushe <shejiayu1998@gmail.com>
2021-09-26 00:56:30 +0800

    Update decoder

--- codeitsuisse/routes/decode.py
+++ codeitsuisse/routes/decode.py
@@ -9,15 +9,48 @@ logger = logging.getLogger(__name__)
 def evaluateDecoder():
     data = request.get_json()
     logging.info("data sent for evaluation {}".format(data))
+    result = {}
+    result["answer"] = solve(data)
+    logging.info("My result :{}".format(result))
+    return jsonify(result)
+
+def solve(data):
     xs = data["possible_values"]
     m = len(xs)
     n = data["num_slots"]
     hist = data["history"]
-    arr = []
+    while True:
+        arr = []
+        for i in range(n):
+            k = random.randint(0, m - 1)
+            arr.append(xs[k])
+        found = True
+        for h in hist:
+            if not score(arr, h):
+                found = False
+                break
+        if found:
+            return arr
+
+def score(arr, h):
+    guess = h["output_received"]
+    s = h["result"]
+    n = len(arr)
+    match = 0
+    mismatch = 0
+    origin_set = {}
+    guess_set = {}
     for i in range(n):
-        k = random.randint(0, m - 1)
-        arr.append(xs[k])
-    result = {}
-    result["answer"] = arr
-    logging.info("My result :{}".format(result))
-    return jsonify(result)
+        if arr[i] == guess[i]:
+            match += 1
+        else:
+            if arr[i] not in origin_set:
+                origin_set[arr[i]] = 0
+            origin_set[arr[i]] += 1
+            if guess[i] not in guess_set:
+                guess_set[guess[i]] = 0
+            guess_set[guess[i]] += 1
+    for k, v in origin_set.items():
+        if k in guess_set:
+            mismatch += min(v, guess_set[k])
+    return match + mismatch * 10 == s
