c7fe5c15c6596f56df4b53701c6ecfb562bdaee2
jiayushe <shejiayu1998@gmail.com>
2021-09-26 11:27:04 +0800

    Add index page

--- App.py
+++ App.py
@@ -1,5 +1,8 @@
 import logging
 from codeitsuisse import app
+from flask import render_template, request, send_from_directory
+import os
+import glob
 
 logger = logging.getLogger(__name__)
 logger = logging.getLogger()
@@ -11,7 +14,16 @@ logger.setLevel(logging.INFO)
 
 @app.route('/', methods=['GET'])
 def default_route():
-    return "Python Template"
+    root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'challenges')
+    paths = glob.glob(os.path.join(root, '*.html'))
+    pages = [path.split('/')[-1] for path in paths]
+    pages.sort()
+    return render_template('index.html', base=request.url + 'challenges/', pages=pages)
+
+@app.route('/challenges/<path:path>', methods=['GET'])
+def challenges(path):
+    root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'challenges')
+    return send_from_directory(root, path)
 
 if __name__ == "__main__":
     logging.info("Starting application ...")
