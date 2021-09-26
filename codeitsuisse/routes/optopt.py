dcc7cac06cb60a7f116cea377c247603c63296bc
jiayushe <shejiayu1998@gmail.com>
2021-09-26 17:37:27 +0800

    Fix optopt

--- codeitsuisse/routes/optopt.py
+++ codeitsuisse/routes/optopt.py
@@ -22,35 +22,32 @@ def solve(data):
         s += truncnorm.expect(lambda x: x, args=(v["min"], v["max"]), loc=v["min"], scale=math.sqrt(v["var"])) * v["weight"]
     exp = s / w
     options = data["options"]
-    profit = []
-    sign = []
-    for opt in options:
+    m = len(options)
+    profit = 0
+    sign = 0
+    index = -1
+    for i in range(m):
+        opt = options[i]
+        cur_profit = 0
+        cur_sign = 0
         if opt["type"] == "call":
-            if exp - opt["strike"] - opt["premium"] > 0.05 * exp:
-                sign.append(1)
-                profit.append(exp - opt["strike"] - opt["premium"])
-            elif exp - opt["strike"] < -0.05 * exp:
-                sign.append(-1)
-                profit.append(opt["premium"])
-            else:
-                sign.append(0)
-                profit.append(0)
+            if exp - opt["strike"] - opt["premium"] > 0:
+                cur_profit = exp - opt["strike"] - opt["premium"]
+                cur_sign = 1
+            elif exp - opt["strike"] < 0:
+                cur_profit = opt["premium"]
+                cur_sign = -1
         else:
-            if opt["strike"] - exp - opt["premium"] > 0.05 * exp:
-                sign.append(1)
-                profit.append(opt["strike"] - exp - opt["premium"])
-            elif opt["strike"] - exp < -0.05 * exp:
-                sign.append(-1)
-                profit.append(opt["premium"])
-            else:
-                sign.append(0)
-                profit.append(0)
-    s_profit = sum(profit)
-    s_cnt = 0
-    ans = []
-    for i in range(len(options) - 1):
-        cnt = int(profit[i] / s_profit * 100)
-        s_cnt += cnt
-        ans.append(cnt * sign[i])
-    ans.append((100 - s_cnt) * sign[-1])
+            if opt["strike"] - exp - opt["premium"] > 0:
+                cur_profit = opt["strike"] - exp - opt["premium"]
+                cur_sign = 1
+            elif opt["strike"] - exp < 0:
+                cur_profit = opt["premium"]
+                cur_sign = -1
+        if cur_profit > profit:
+            profit = cur_profit
+            sign = cur_sign
+            index = i
+    ans = [0 for i in range(m)]
+    ans[index] = sign * 100
     return ans
